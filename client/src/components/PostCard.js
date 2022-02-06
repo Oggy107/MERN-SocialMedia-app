2aa9c61fe96af49c8d86838b3ceb8834b5d9869b
Oggy107 <urmalsingh10@gmail.com>
2022-02-06 15:30:57 +0530

    Added: show likes

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,8 +3,25 @@ import moment from 'moment';
 import { Card, Image, Button, Label, Icon } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
 
+import { UserContext } from '../context/user';
+
 const PostCard = ({post: { _id, body, createdAt, user, comments, likes }}) => {
+    const { state: userState } = React.useContext(UserContext);
     const navigate = useNavigate();
+
+    const isLiked = () => {
+        let liked = false;
+
+        if (likes.length > 0) {
+            likes.forEach(like => {
+                if (like.user._id === userState.user._id) {
+                    liked = true;
+                }
+            })
+        }
+
+        return liked;
+    }
 
     const handleCardClick = () => {
         navigate('/home/posts/' + _id);
@@ -35,7 +52,7 @@ const PostCard = ({post: { _id, body, createdAt, user, comments, likes }}) => {
             <Card.Content extra>
                 {/* <div className='ui two buttons'> */}
                     <Button as='div' labelPosition='right'>
-                        <Button basic color='teal' onClick={likePost}>
+                        <Button basic={!isLiked()} color='teal' onClick={likePost}>
                             <Icon name='heart' size='large' />
                         </Button>
                         <Label basic color='teal' pointing='left'>
